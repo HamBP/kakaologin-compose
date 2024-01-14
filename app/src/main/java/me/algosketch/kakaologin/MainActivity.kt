4b56ba24588264467fda7f2423df68db45860cb4
algosketch <algosketch@gmail.com>
2024-01-15 02:14:21 +0900

    feat : 로그인 버튼 만들기

--- app/src/main/java/me/algosketch/kakaologin/MainActivity.kt
+++ app/src/main/java/me/algosketch/kakaologin/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,25 +23,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "카카오로 로그인")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KakaoLoginTheme {
-        Greeting("Android")
     }
 }
